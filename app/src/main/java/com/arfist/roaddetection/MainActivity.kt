547946a50e52a3c79dac2648bc107ec7082aba95
sprkzoff <sprkzoff@gmail.com>
2021-02-12 08:58:48 +0700

    average line implemented but line coordinates still bug

--- app/src/main/java/com/arfist/roaddetection/MainActivity.kt
+++ app/src/main/java/com/arfist/roaddetection/MainActivity.kt
@@ -115,24 +115,24 @@ class MainActivity : AppCompatActivity() {
 
                 Log.d("Houghlines",lines.rows().toString())
 
-                for (i in 0 until lines.rows()) {
-                    val points = lines[i, 0]
-                    var x1 = points[0]
-                    var y1 = points[1]
-                    var x2 = points[2]
-                    var y2 = points[3]
-                    val Angle: Double = atan2(y2 - y1, x2 - x1) * 180.0 / PI
+                var result_lines = img?.let { average_slope_intercept(it,lines) }
 
-                    // cut out some lines
-                    if( ( -90.0 <= Angle && Angle <= -60.0) || (Angle in 60.0..90.0)) {
-                        continue
-                    }
-                    Log.d("Angle", Angle.toString())
-                    val pt1 = Point(x1, y1)
-                    val pt2 = Point(x2, y2)
-                    //Drawing lines on an image
-                    Imgproc.line(resultMat, pt1, pt2, Scalar(255.0, 0.0, 0.0), 2)
-                }
+                // plot left
+                var left = result_lines?.get(0)
+                Log.d("Left line (x1,y1,x2,y2)",left.toString())
+                var pt1 = left?.get(0)?.let { Point(it.toDouble(), left[1].toDouble()) }
+                var pt2 = left?.get(2)?.let { Point(it.toDouble(), left[3].toDouble()) }
+                //Drawing lines on an image
+                Imgproc.line(resultMat, pt1, pt2, Scalar(255.0, 0.0, 0.0), 2)
+
+                // plot right
+                var right = result_lines?.get(1)
+                Log.d("Right line (x1,y1,x2,y2)",right.toString())
+                pt1 = right?.get(0)?.let { Point(it.toDouble(), right[1].toDouble()) }
+                pt2 = right?.get(2)?.let { Point(it.toDouble(), right[3].toDouble()) }
+                //Drawing lines on an image
+                Imgproc.line(resultMat, pt1, pt2, Scalar(255.0, 0.0, 0.0), 2)
+
 
                 val resultBitmap = img?.copy(Bitmap.Config.RGB_565, true)
                 Utils.matToBitmap(resultMat, resultBitmap)
@@ -216,7 +216,59 @@ class MainActivity : AppCompatActivity() {
         Core.bitwise_and(img, mask, dst)
         Log.d("ROI", "dst >> "+ dst.width().toString() + ", " + dst.height().toString() + ", " + dst.type().toString())
         return dst
+    }
 
+    private fun average_slope_intercept(bitmap : Bitmap, lines: Mat): ArrayList<ArrayList<Int>> {
+        var left_slope_mean = 0.0
+        var right_slope_mean = 0.0
+        var left_intercept_mean = 0.0
+        var right_intercept_mean = 0.0
+        var left_n = 0
+        var right_n = 0
+        for(i in 0 until lines.rows()) {
+            val points = lines[i, 0]
+            var x1 = points[0]
+            var y1 = points[1]
+            var x2 = points[2]
+            var y2 = points[3]
+
+            // cut out some lines
+            val Angle: Double = atan2(y2 - y1, x2 - x1) * 180.0 / PI
+            if( ( -90.0 <= Angle && Angle <= -60.0) || (Angle in 60.0..90.0)) {
+                continue
+            }
+            Log.d("Angle", Angle.toString())
+
+            var slope = (y2-y1)/(x2-x1)
+            var intercept = y1 - (slope * x1)
+            Log.d("average_slope_intercept","slope: " + slope.toString() + ", intercept: " + intercept.toString())
+            if(slope < 0) {
+                left_slope_mean += slope
+                left_intercept_mean += intercept
+                left_n +=1
+            }
+            else {
+                right_slope_mean += slope
+                right_intercept_mean += intercept
+                right_n +=1
+            }
+        }
+        left_slope_mean /= left_n
+        left_intercept_mean /= left_n
+        right_slope_mean /= right_n
+        right_intercept_mean /= right_n
+        var left_line = make_coor(bitmap,left_slope_mean,left_intercept_mean)
+        var right_line = make_coor(bitmap,right_slope_mean,right_intercept_mean)
+        return arrayListOf(left_line,right_line)
+    }
+
+    private fun make_coor(bitmap: Bitmap, slope: Double, intercept: Double): ArrayList<Int> {
+        var y1 = bitmap.width
+        var y2 = (y1 * (4/5))
+        var x1 = ((y1 - intercept) / slope).toInt()
+        var x2 = ((y2 - intercept) / slope).toInt()
+
+        return arrayListOf(x1, y1, x2, y2)
     }
 
     companion object {
